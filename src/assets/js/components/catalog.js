d367dbd31a9e2a2740504da3895dcefc63f6b2bf
Khakim <khakim.arli@bk.ru>
2026-06-09 22:37:30 +0600

    Сделал каталог

--- src/assets/js/components/catalog.js
+++ src/assets/js/components/catalog.js
@@ -5,6 +5,7 @@ if (homeCatalog) {
     const tabs = tabsWrapper.querySelectorAll('.catalog__tab');
     const showAllATabsButton = homeCatalog.querySelector('.catalog__open');
     const tabRowsCount = new Set();
+    const catalogGrid = document.querySelector('.catalog__grid');
 
     // Скрытие табов на мобилке
     if (isMobile) {
@@ -28,5 +29,80 @@ if (homeCatalog) {
     }
 
     // Переключение табов
-    tabs[0].classList.add('active');
+    activateTab(tabs[0]);
+
+    tabsWrapper.addEventListener('click', (e) => {
+        const activeTab = e.target.closest('.catalog__tab');
+        if (activeTab) {
+            activateTab(activeTab);
+        }
+    })
+    
+    async function activateTab(tab) {
+        // Переключение класса active
+        if (tab.classList.contains('active')) {
+            return;
+        }
+        tabs.forEach(tab => {
+            tab.classList.remove('active');
+        })
+        tab.classList.add('active');
+
+        
+        const targetMark = tab.dataset.mark;
+
+        // Заккоментировать или удалить. Пока для тренировки
+        const gridContent = catalogGrid.innerHTML;
+        // Заккоментировать или удалить. Пока для тренировки
+
+        try {
+            catalogGrid.innerHTML = '';
+            catalogGrid.classList.add('loading');
+
+            // Заккоментировать или удалить. Пока для тренировки
+            const response = await new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    console.log(targetMark);
+                    resolve({
+                        ok: true,
+                        status: 200,
+                        text() {
+                            return Promise.resolve(gridContent);
+                        }
+                    });
+                }, 1500);
+            })
+            // Заккоментировать или удалить. Пока для тренировки
+
+
+
+            // Расскомментировать перед продом
+            // const response = await fetch('/wp-admin/admin-ajax.php', {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/x-www-form-urlencoded'
+            //     },
+            //     body: new URLSearchParams({
+            //         action: 'load_cars',
+            //         brand: targetMark
+            //     })
+            // });
+            // Расскомментировать перед продом
+
+
+
+            if (!response.ok) {
+                throw new Error(`HTTP error: ${response.status}`);
+            }
+
+            const html = await response.text();
+            catalogGrid.innerHTML = html;
+
+        } catch (error) {
+            console.error(error);
+        } finally {
+            catalogGrid.classList.remove('loading');
+        }
+    }
+
 }
